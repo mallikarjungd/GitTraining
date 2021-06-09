30ee6b2795d2b5245d2b0f7003231903ffa69af8
mallikarjun <mallikarjunaganji@eurofins.com>
2021-06-09 10:14:47 +0530

    Add project files.

---
+++ WebAndLoadTestProject9/ExtractToken.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.WebTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAndLoadTestProject9
+{
+    public class ExtractToken : ExtractionRule
+    {
+        public override void Extract(object sender, ExtractionEventArgs e)
+        {
+            WebHeaderCollection list = e.Response.Headers;
+
+            string locationHeader = e.WebTest.Context["token"].ToString();
+
+            string location = locationHeader.ToString().Trim();
+            int firstIndex = location.IndexOf('=');
+            int lastIndex = location.IndexOf("&token_type");
+            string token = location.Substring(firstIndex+1, lastIndex - firstIndex-1);
+            e.WebTest.Context["BearerToken"] = token;
+        }
+    }
+}
